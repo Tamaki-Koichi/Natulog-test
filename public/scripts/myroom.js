df8a76f4f53ba76b685ca8eddf6a381821152569
Mari the Islander <69796853+Caitlyn1064@users.noreply.github.com>
2021-10-22 11:28:11 +0900

    Add files via upload

--- public/scripts/myroom.js
+++ public/scripts/myroom.js
@@ -1,3 +1,97 @@
+// 'use strict';
+
+// var db = firebase.firestore();
+// var citiesRef = db.collection("messages");
+// var addDoc;
+
+// // Add a new document in collection "cities"
+// db.collection("messages").doc("LA").set({
+//   name: getUserName(),
+//   text: messageText,
+//   timestamp: serverTimestamp()
+// })
+// .then(() => {
+//   console.log("Document successfully written!");
+// })
+// .catch((error) => {
+//   console.error("Error writing document: ", error);
+// });
+
+// var cityRef = db.collection('cities').doc('BJ');
+
+// var setWithMerge = cityRef.set({
+//     capital: true
+// }, { merge: true });
+
+// var docData = {
+//   stringExample: "Hello world!",
+//   booleanExample: true,
+//   numberExample: 3.14159265,
+//   dateExample: firebase.firestore.Timestamp.fromDate(new Date("December 10, 1815")),
+//   arrayExample: [5, true, "hello"],
+//   nullExample: null,
+//   objectExample: {
+//       a: 5,
+//       b: {
+//           nested: "foo"
+//       }
+//   }
+// };
+// db.collection("data").doc("one").set(docData).then(() => {
+//   console.log("Document successfully written!");
+// });
+
+// class City {
+//   constructor (name, state, country ) {
+//       this.name = name;
+//       this.state = state;
+//       this.country = country;
+//   }
+//   toString() {
+//       return this.name + ', ' + this.state + ', ' + this.country;
+//   }
+// }
+
+// // Firestore data converter
+// var cityConverter = {
+//   toFirestore: function(city) {
+//       return {
+//           name: city.name,
+//           state: city.state,
+//           country: city.country
+//           };
+//   },
+//   fromFirestore: function(snapshot, options){
+//       const data = snapshot.data(options);
+//       return new City(data.name, data.state, data.country);
+//   }
+// };
+
+// // Set with cityConverter
+// db.collection("cities").doc("LA")
+//   .withConverter(cityConverter)
+//   .set(new City("Los Angeles", "CA", "USA"));
+
+// // Add a new document with a generated id.
+// db.collection("cities").add({
+//   name: "Tokyo",
+//   country: "Japan"
+// })
+// .then((docRef) => {
+//   console.log("Document written with ID: ", docRef.id);
+// })
+// .catch((error) => {
+//   console.error("Error adding document: ", error);
+// });
+
+// Get a list of messages from your database
+async function getmessages(db) {
+  const messagesCol = collection(db, 'messages');
+  const messagesSnapshot = await getDocs(messagesCol);
+  const messagesList = messagesSnapshot.docs.map(doc => doc.data());
+  return messagesList;
+}
+
 var messageListElement;
 var messageFormElement;
 var messageInputElement;
@@ -6,8 +100,6 @@ var imageFormElement;
 var mediaCaptureElement;
 var userPicElement;
 var userNameElement;
-var signInButtonElement;
-var signOutButtonElement;
 var signInSnackbarElement;
 
 
@@ -35,15 +127,14 @@ function initApp() {
 	messageInputElement =  document.getElementById('message');
 	messageInputElement.addEventListener('keyup', toggleButton);
 
+
+
 	messageInputElement.addEventListener('change', toggleButton);
 
 	messageFormElement = document.getElementById('submit');
 	messageFormElement.addEventListener('click', onMessageFormSubmit, false);
 
 
-// 	document.getElementById('keyup').addEventListener('click', toggleButton, false);
-// 	// console.log('initApp');
-// 
 }
 
 window.addEventListener = function() {
@@ -74,6 +165,126 @@ function toggleButton() {
   }
 }
 
+// Saves a new message on the Cloud Firestore.
+async function saveMessage(messageText) {
+  // TODO 7: Push a new message to Cloud Firestore.
+  // Add a new message entry to the Firebase database.
+  try {
+    await addDoc(collection(getFirestore(), 'messages'), {
+      name: getUserName(),
+      text: messageText,
+      // profilePicUrl: getProfilePicUrl(),
+      timestamp: serverTimestamp()
+    });
+  }
+  catch(error) {
+    console.error('Error writing new message to Firebase Database', error);
+  }
+}
+
+// // Loads chat messages history and listens for upcoming ones.
+function loadMessages() {
+  // TODO 8: Load and listen for new messages.
+  // Create the query to load the last 12 messages and listen for new ones.
+  const recentMessagesQuery = query(collection(getFirestore(), 'messages'), orderBy('timestamp', 'desc'), limit(12));
+  
+  // Start listening to the query.
+  onSnapshot(recentMessagesQuery, function(snapshot) {
+    snapshot.docChanges().forEach(function(change) {
+      if (change.type === 'removed') {
+        deleteMessage(change.doc.id);
+      } else {
+        var message = change.doc.data();
+        displayMessage(change.doc.id, message.timestamp, message.name,
+                      message.text, message.profilePicUrl, message.imageUrl);
+      }
+    });
+  });
+}
+
+// Saves a new message containing an image in Firebase.
+// This first saves the image in Firebase storage.
+async function saveImageMessage(file) {
+  // TODO 9: Posts a new image as a message.
+  try {
+    // 1 - We add a message with a loading icon that will get updated with the shared image.
+    const messageRef = await db.addDoc(collection(getFirestore(), 'messages'), {
+      name: getUserName(),
+      imageUrl: LOADING_IMAGE_URL,
+      profilePicUrl: getProfilePicUrl(),
+      timestamp: serverTimestamp()
+    });
+
+    // 2 - Upload the image to Cloud Storage.
+    const filePath = `${getAuth().currentUser.uid}/${messageRef.id}/${file.name}`;
+    const newImageRef = ref(getStorage(), filePath);
+    const fileSnapshot = await uploadBytesResumable(newImageRef, file);
+    
+    // 3 - Generate a public URL for the file.
+    const publicImageUrl = await getDownloadURL(newImageRef);
+
+    // 4 - Update the chat message placeholder with the image's URL.
+    await updateDoc(messageRef,{
+      imageUrl: publicImageUrl,
+      storageUri: fileSnapshot.metadata.fullPath
+    });
+  } catch (error) {
+    console.error('There was an error uploading a file to Cloud Storage:', error);
+  }
+}
+
+// Saves the messaging device token to Cloud Firestore.
+async function saveMessagingDeviceToken() {
+  // TODO 10: Save the device token in Cloud Firestore
+  try {
+    const currentToken = await getToken(getMessaging());
+    if (currentToken) {
+      console.log('Got FCM device token:', currentToken);
+      // Saving the Device Token to Cloud Firestore.
+      const tokenRef = doc(getFirestore(), 'fcmTokens', currentToken);
+      await setDoc(tokenRef, { uid: getAuth().currentUser.uid });
+
+      // This will fire when a message is received while the app is in the foreground.
+      // When the app is in the background, firebase-messaging-sw.js will receive the message instead.
+      onMessage(getMessaging(), (message) => {
+        console.log(
+          'New foreground notification from Firebase Messaging!',
+          message.notification
+        );
+      });
+    } else {
+      // Need to request permissions to show notifications.
+      requestNotificationsPermissions();
+    }
+  } catch(error) {
+    console.error('Unable to get messaging token.', error);
+  };
+}
+
+// Triggered when a file is selected via the media picker.
+function onMediaFileSelected(event) {
+  event.preventDefault();
+  var file = event.target.files[0];
+
+  // Clear the selection in the file picker input.
+  imageFormElement.reset();
+
+  // Check if the file is an image.
+  if (!file.type.match('image.*')) {
+    var data = {
+      message: 'You can only share images',
+      timeout: 2000,
+    };
+    signInSnackbarElement.MaterialSnackbar.showSnackbar(data);
+    return;
+  }
+  // Check if the user is signed-in
+  if (checkSignedInWithMessage()) {
+    saveImageMessage(file);
+  }
+}
+
+
 // Triggered when the send new message form is submitted.
 function onMessageFormSubmit(e) {
   e.preventDefault();
@@ -87,19 +298,190 @@ function onMessageFormSubmit(e) {
   }
 }
 
-// Saves a new message on the Cloud Firestore.
-async function saveMessage(messageText) {
-  // TODO 7: Push a new message to Cloud Firestore.
-  // Add a new message entry to the Firebase database.
+// Triggers when the auth state change for instance when the user signs-in or signs-out.
+function authStateObserver(user) {
+  if (user) {
+    // User is signed in!
+    // Get the signed-in user's profile pic and name.
+    var profilePicUrl = getProfilePicUrl();
+    var userName = getUserName();
+
+    // Set the user's profile pic and name.
+    userPicElement.style.backgroundImage =
+      'url(' + addSizeToGoogleProfilePic(profilePicUrl) + ')';
+    userNameElement.textContent = userName;
+
+    // // Show user's profile and sign-out button.
+    // userNameElement.removeAttribute('hidden');
+    // userPicElement.removeAttribute('hidden');
+    // signOutButtonElement.removeAttribute('hidden');
+
+    // // Hide sign-in button.
+    // signInButtonElement.setAttribute('hidden', 'true');
+
+    // We save the Firebase Messaging Device token and enable notifications.
+    saveMessagingDeviceToken();
+  // } else {
+  //   // User is signed out!
+  //   // Hide user's profile and sign-out button.
+  //   userNameElement.setAttribute('hidden', 'true');
+  //   userPicElement.setAttribute('hidden', 'true');
+  //   signOutButtonElement.setAttribute('hidden', 'true');
+
+  //   // Show sign-in button.
+  //   signInButtonElement.removeAttribute('hidden');
+  }
+}
+
+// Returns true if user is signed-in. Otherwise false and displays a message.
+function checkSignedInWithMessage() {
+  // Return true if the user is signed in Firebase
+  if (isUserSignedIn()) {
+    return true;
+  }
+
+  // Display a message to the user using a Toast.
+  var data = {
+    message: 'You must sign-in first',
+    timeout: 2000,
+  };
+  signInSnackbarElement.MaterialSnackbar.showSnackbar(data);
+  return false;
+}
+
+// Resets the given MaterialTextField.
+function resetMaterialTextfield(element) {
+  element.value = '';
+  element.parentNode.MaterialTextfield.boundUpdateClassesHandler();
+}
+
+async function saveMessagingDeviceToken() {
+  // TODO 10: Save the device token in Cloud Firestore
   try {
-    await addDoc(collection(getFirestore(), 'messages'), {
-      name: getUserName(),
-      text: messageText,
-      profilePicUrl: getProfilePicUrl(),
-      timestamp: serverTimestamp()
+    const currentToken = await getToken(getMessaging());
+    if (currentToken) {
+      console.log('Got FCM device token:', currentToken);
+      // Saving the Device Token to Cloud Firestore.
+      const tokenRef = doc(getFirestore(), 'fcmTokens', currentToken);
+      await setDoc(tokenRef, { uid: getAuth().currentUser.uid });
+
+      // This will fire when a message is received while the app is in the foreground.
+      // When the app is in the background, firebase-messaging-sw.js will receive the message instead.
+      onMessage(getMessaging(), (message) => {
+        console.log(
+          'New foreground notification from Firebase Messaging!',
+          message.notification
+        );
+      });
+    } else {
+      // Need to request permissions to show notifications.
+      requestNotificationsPermissions();
+    }
+  } catch(error) {
+    console.error('Unable to get messaging token.', error);
+  };
+}
+// A loading image URL.
+var LOADING_IMAGE_URL = 'https://www.google.com/images/spin-32.gif?a';
+
+// Delete a Message from the UI.
+function deleteMessage(id) {
+  var div = document.getElementById(id);
+  // If an element for that message exists we delete it.
+  if (div) {
+    div.parentNode.removeChild(div);
+  }
+}
+
+// Template for messages.
+var MESSAGE_TEMPLATE =
+  '<div class="message-container">' +
+  '<div class="spacing"><div class="pic"></div></div>' +
+  '<div class="message"></div>' +
+  '<div class="name"></div>' +
+  '</div>';
+
+// Adds a size to Google Profile pics URLs.
+function addSizeToGoogleProfilePic(url) {
+  if (url.indexOf('googleusercontent.com') !== -1 && url.indexOf('?') === -1) {
+    return url + '?sz=150';
+  }
+  return url;
+}
+
+function createAndInsertMessage(id, timestamp) {
+  const container = document.createElement('div');
+  container.innerHTML = MESSAGE_TEMPLATE;
+  const div = container.firstChild;
+  div.setAttribute('id', id);
+
+  // If timestamp is null, assume we've gotten a brand new message.
+  // https://stackoverflow.com/a/47781432/4816918
+  timestamp = timestamp ? timestamp.toMillis() : Date.now();
+  div.setAttribute('timestamp', timestamp);
+
+  // figure out where to insert new message
+  const existingMessages = messageListElement.children;
+  if (existingMessages.length === 0) {
+    messageListElement.appendChild(div);
+  } else {
+    let messageListNode = existingMessages[0];
+
+    while (messageListNode) {
+      const messageListNodeTime = messageListNode.getAttribute('timestamp');
+
+      if (!messageListNodeTime) {
+        throw new Error(
+          `Child ${messageListNode.id} has no 'timestamp' attribute`
+        );
+      }
+
+      if (messageListNodeTime > timestamp) {
+        break;
+      }
+
+      messageListNode = messageListNode.nextSibling;
+    }
+
+    messageListElement.insertBefore(div, messageListNode);
+  }
+
+  return div;
+}
+
+// Displays a Message in the UI.
+function displayMessage(id, timestamp, name, text, picUrl, imageUrl) {
+  var div =
+    document.getElementById(id) || createAndInsertMessage(id, timestamp);
+
+  // profile picture
+  if (picUrl) {
+    div.querySelector('.pic').style.backgroundImage =
+      'url(' + addSizeToGoogleProfilePic(picUrl) + ')';
+  }
+
+  div.querySelector('.name').textContent = name;
+  var messageElement = div.querySelector('.message');
+
+  if (text) {
+    // If the message is text.
+    messageElement.textContent = text;
+    // Replace all line breaks by <br>.
+    messageElement.innerHTML = messageElement.innerHTML.replace(/\n/g, '<br>');
+  } else if (imageUrl) {
+    // If the message is an image.
+    var image = document.createElement('img');
+    image.addEventListener('load', function () {
+      messageListElement.scrollTop = messageListElement.scrollHeight;
     });
+    image.src = imageUrl + '&' + new Date().getTime();
+    messageElement.innerHTML = '';
+    messageElement.appendChild(image);
   }
-  catch(error) {
-    console.error('Error writing new message to Firebase Database', error);
-  }
+  // Show the card fading-in and scroll to view the new message.
+  setTimeout(function () {
+    div.classList.add('visible');
+  }, 1);
+  messageListElement.scrollTop = messageListElement.scrollHeight;
+  messageInputElement.focus();
 }
