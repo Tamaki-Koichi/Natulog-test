bee99de02557953caaaa437d66828f55a829834c
ittiy <68366168+Tamaki-Koichi@users.noreply.github.com>
2021-11-29 16:28:55 +0900

    ポイント消費、チーム・キャラクター転生の実装

--- kouichiPublic/scripts/avatorSelect.js
+++ kouichiPublic/scripts/avatorSelect.js
@@ -69,8 +69,62 @@ window.onload = function() {
         }
     })
 };
-
+//画像出力
 window.onload = function dispPoint() {
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+                var uid = user.uid;
+                docRef.doc(uid).get().then((doc) => {
+                    if (doc.exists) {
+                        var myTeamNum = doc.data().teamNumber;
+                        if (myTeamNum == 1) {
+                            var avator1 = document.getElementById("avator1");
+                            avator1.src = "./images/avatorpic/4.png";
+                            var avator2 = document.getElementById("avator2");
+                            avator2.src = "./images/avatorpic/5.png";
+                            var avator3 = document.getElementById("avator3");
+                            avator3.src = "./images/avatorpic/6.png";
+                            var avator4 = document.getElementById("avator4");
+                            avator4.src = "./images/avatorpic/7.png";
+                            var avator5 = document.getElementById("avator5");
+                            avator5.src = "./images/avatorpic/8.png";
+                        } else if (myTeamNum == 2) {
+                            var avator1 = document.getElementById("avator1");
+                            avator1.src = "./images/avatorpic/9.png";
+                            var avator2 = document.getElementById("avator2");
+                            avator2.src = "./images/avatorpic/10.png";
+                            var avator3 = document.getElementById("avator3");
+                            avator3.src = "./images/avatorpic/11.png";
+                            var avator4 = document.getElementById("avator4");
+                            avator4.src = "./images/avatorpic/12.png";
+                            var avator5 = document.getElementById("avator5");
+                            avator5.src = "./images/avatorpic/13.png";
+                        } else if (myTeamNum == 3) {
+                            var avator1 = document.getElementById("avator1");
+                            avator1.src = "./images/avatorpic/14.png";
+                            var avator2 = document.getElementById("avator2");
+                            avator2.src = "./images/avatorpic/15.png";
+                            var avator3 = document.getElementById("avator3");
+                            avator3.src = "./images/avatorpic/16.png";
+                            var avator4 = document.getElementById("avator4");
+                            avator4.src = "./images/avatorpic/17.png";
+                            var avator5 = document.getElementById("avator5");
+                            avator5.src = "./images/avatorpic/18.png";
+                            var avator6 = document.getElementById("avator6");
+                            avator6.src = "./images/avatorpic/19.png";
+                        }
+                    }
+                })
+            }
+        })
+        const check = false;
+        let form = document.forms[0];
+        let image = form.image;
+        console.log(image);
+
+    }
+    //キャラクター選択分岐
+function select1() {
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
             var uid = user.uid;
@@ -78,189 +132,158 @@ window.onload = function dispPoint() {
                 if (doc.exists) {
                     var myTeamNum = doc.data().teamNumber;
                     if (myTeamNum == 1) {
-                        var avator1 = document.getElementById("avator1");
-                        avator1.src = "./images/avatorpic/4.png";
-                        var avator2 = document.getElementById("avator2");
-                        avator2.src = "./images/avatorpic/5.png";
-                        var avator3 = document.getElementById("avator3");
-                        avator3.src = "./images/avatorpic/6.png";
-                        var avator4 = document.getElementById("avator4");
-                        avator4.src = "./images/avatorpic/7.png";
-                        var avator5 = document.getElementById("avator5");
-                        avator5.src = "./images/avatorpic/8.png";
+                        var check = window.confirm("ビッグフットに転生しますか？");
+                        if (check) {
+                            location.href = "./avatorTransfer/tr4.html";
+                        }
                     } else if (myTeamNum == 2) {
-                        var avator1 = document.getElementById("avator1");
-                        avator1.src = "./images/avatorpic/9.png";
-                        var avator2 = document.getElementById("avator2");
-                        avator2.src = "./images/avatorpic/10.png";
-                        var avator3 = document.getElementById("avator3");
-                        avator3.src = "./images/avatorpic/11.png";
-                        var avator4 = document.getElementById("avator4");
-                        avator4.src = "./images/avatorpic/12.png";
-                        var avator5 = document.getElementById("avator5");
-                        avator5.src = "./images/avatorpic/13.png";
+                        var check = window.confirm("イルカに転生しますか？");
+                        if (check) {
+                            location.href = "./avatorTransfer/tr9.html";
+                        }
                     } else if (myTeamNum == 3) {
-                        var avator1 = document.getElementById("avator1");
-                        avator1.src = "./images/avatorpic/14.png";
-                        var avator2 = document.getElementById("avator2");
-                        avator2.src = "./images/avatorpic/15.png";
-                        var avator3 = document.getElementById("avator3");
-                        avator3.src = "./images/avatorpic/16.png";
-                        var avator4 = document.getElementById("avator4");
-                        avator4.src = "./images/avatorpic/17.png";
-                        var avator5 = document.getElementById("avator5");
-                        avator5.src = "./images/avatorpic/18.png";
-                        var avator6 = document.getElementById("avator6");
-                        avator6.src = "./images/avatorpic/19.png";
+                        var check = window.confirm("ドラゴンに転生しますか？");
+                        if (check) {
+                            location.href = "./avatorTransfer/tr14.html";
+                        }
                     }
                 }
             })
         }
     })
-    const check = false;
-    let form = document.forms[0];
-    let image = form.image;
-    console.log(image);
-    //「送信」ボタン取得
-    let submit = form.submit;
+}
 
-    //「送信」ボタンがクリックされたら
-    submit.addEventListener('click', () => {
-
-        //ラジオボタンの数分繰り返し処理
-        for (let i = 0; i < image.length; i++) {
-            //ラジオボタンがチェック状態であれば
-            if (image[i].checked) {
-                //コンソールに値を出力する
-                firebase.auth().onAuthStateChanged(function(user) {
-                    if (user) { //この階層まではconsole.logが動いている
-                        var uid = user.uid;
-                        console.log(uid);
-                        docRef.doc(uid).get().then((doc) => {
-                            if (doc.exists) {
-                                var myTeamNum = doc.data().teamNumber;
-                                console.log("ここが読み込まれていたら言って！" + myTeamNum);
-                                if (myTeamNum == 1) {
-                                    if (i == 0) {
-                                        docRef.doc(uid).update({
-                                            avator: 'ビッグフット',
-                                            avatirNumber: 4
-                                        });
-                                        console.log(i);
-                                    } else if (i == 1) {
-                                        docRef.doc(uid).update({
-                                            avator: 'ブタ',
-                                            avatirNumber: 5
-                                        });
-                                        console.log(i);
-                                    } else if (i == 2) {
-                                        docRef.doc(uid).update({
-                                            avator: 'ネコ',
-                                            avatirNumber: 6
-                                        });
-                                        console.log(i);
-                                    } else if (i == 3) {
-                                        docRef.doc(uid).update({
-                                            avator: 'イヌ',
-                                            avatirNumber: 7
-                                        });
-                                        console.log(i);
-                                    } else if (i == 4) {
-                                        docRef.doc(uid).update({
-                                            avator: 'ライオン',
-                                            avatirNumber: 8
-                                        });
-                                        console.log(i);
-                                    }
-                                } else if (myTeamNum == 2) {
-                                    if (i == 0) {
-                                        docRef.doc(uid).update({
-                                            avator: 'イルカ',
-                                            avatirNumber: 9
-                                        });
-                                        console.log(i);
-                                    } else if (i == 1) {
-                                        docRef.doc(uid).update({
-                                            avator: '半魚人',
-                                            avatirNumber: 10
-                                        });
-                                        console.log(i);
-                                    } else if (i == 2) {
-                                        docRef.doc(uid).update({
-                                            avator: 'カクレクマノミ',
-                                            avatirNumber: 11
-                                        });
-                                        console.log(i);
-                                    } else if (i == 3) {
-                                        docRef.doc(uid).update({
-                                            avator: 'タツノオトシゴ',
-                                            avatirNumber: 12
-                                        });
-                                        console.log(i);
-                                    } else if (i == 4) {
-                                        docRef.doc(uid).update({
-                                            avator: 'タコ',
-                                            avatirNumber: 13
-                                        });
-                                        console.log(i);
-                                    }
-                                } else if (myTeamNum == 3) {
-                                    if (i == 0) {
-                                        docRef.doc(uid).update({
-                                            avator: 'ドラゴン',
-                                            avatirNumber: 14
-                                        });
-                                        console.log(i);
-                                    } else if (i == 1) {
-                                        docRef.doc(uid).update({
-                                            avator: 'フラミンゴ',
-                                            avatirNumber: 15
-                                        });
-                                        console.log(i);
-                                    } else if (i == 2) {
-                                        docRef.doc(uid).update({
-                                            avator: 'ハト',
-                                            avatirNumber: 16
-                                        });
-                                        console.log(i);
-                                    } else if (i == 3) {
-                                        docRef.doc(uid).update({
-                                            avator: 'クジャク',
-                                            avatirNumber: 17
-                                        });
-                                        console.log(i);
-                                    } else if (i == 4) {
-                                        docRef.doc(uid).update({
-                                            avator: 'インコ',
-                                            avatirNumber: 18
-                                        });
-                                        console.log(i);
-                                    } else if (i == 5) {
-                                        docRef.doc(uid).update({
-                                            avator: トンビ,
-                                            avatirNumber: 19
-                                        });
-                                        console.log(i);
-                                    }
-                                }
-                                check = true;
-                            }
-                            console.log("認識されてる？！本当に？！");
-                        })
-                        console.log("ここは認識されてる？" + i);
+function select2() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            var uid = user.uid;
+            docRef.doc(uid).get().then((doc) => {
+                if (doc.exists) {
+                    var myTeamNum = doc.data().teamNumber;
+                    if (myTeamNum == 1) {
+                        var check = window.confirm("ブタに転生しますか？");
+                        if (check) {
+                            location.href = "./avatorTransfer/tr5.html";
+                        }
+                    } else if (myTeamNum == 2) {
+                        var check = window.confirm("半魚人に転生しますか？");
+                        if (check) {
+                            location.href = "./avatorTransfer/tr10.html";
+                        }
+                    } else if (myTeamNum == 3) {
+                        var check = window.confirm("フラミンゴに転生しますか？");
+                        if (check) {
+                            location.href = "./avatorTransfer/tr15.html";
+                        }
                     }
-                })
-
-                console.log("認識されたvalueは" + i);
-
-            }
+                }
+            })
         }
+    })
+}
 
-        if (check == true) {
-            location.href = 'selectTrance.html';
-        } else {
-            location.href = 'selectCharactor.html';
+function select3() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            var uid = user.uid;
+            docRef.doc(uid).get().then((doc) => {
+                if (doc.exists) {
+                    var myTeamNum = doc.data().teamNumber;
+                    if (myTeamNum == 1) {
+                        var check = window.confirm("ネコに転生しますか？");
+                        if (check) {
+                            location.href = "./avatorTransfer/tr6.html";
+                        }
+                    } else if (myTeamNum == 2) {
+                        var check = window.confirm("カクレクマノミに転生しますか？");
+                        if (check) {
+                            location.href = "./avatorTransfer/tr11.html";
+                        }
+                    } else if (myTeamNum == 3) {
+                        var check = window.confirm("ハトに転生しますか？");
+                        if (check) {
+                            location.href = "./avatorTransfer/tr16.html";
+                        }
+                    }
+                }
+            })
         }
-    }, false);
+    })
+}
 
+function select4() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            var uid = user.uid;
+            docRef.doc(uid).get().then((doc) => {
+                if (doc.exists) {
+                    var myTeamNum = doc.data().teamNumber;
+                    if (myTeamNum == 1) {
+                        var check = window.confirm("イヌに転生しますか？");
+                        if (check) {
+                            location.href = "./avatorTransfer/tr7.html";
+                        }
+                    } else if (myTeamNum == 2) {
+                        var check = window.confirm("タツノオトシゴに転生しますか？");
+                        if (check) {
+                            location.href = "./avatorTransfer/tr12.html";
+                        }
+                    } else if (myTeamNum == 3) {
+                        var check = window.confirm("クジャクに転生しますか？");
+                        if (check) {
+                            location.href = "./avatorTransfer/tr17.html";
+                        }
+                    }
+                }
+            })
+        }
+    })
+}
+
+function select5() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            var uid = user.uid;
+            docRef.doc(uid).get().then((doc) => {
+                if (doc.exists) {
+                    var myTeamNum = doc.data().teamNumber;
+                    if (myTeamNum == 1) {
+                        var check = window.confirm("ライオンに転生しますか？");
+                        if (check) {
+                            location.href = "./avatorTransfer/tr8.html";
+                        }
+                    } else if (myTeamNum == 2) {
+                        var check = window.confirm("タコに転生しますか？");
+                        if (check) {
+                            location.href = "./avatorTransfer/tr13.html";
+                        }
+                    } else if (myTeamNum == 3) {
+                        var check = window.confirm("インコに転生しますか？");
+                        if (check) {
+                            location.href = "./avatorTransfer/tr18.html";
+                        }
+                    }
+                }
+            })
+        }
+    })
+}
+
+function select6() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            var uid = user.uid;
+            docRef.doc(uid).get().then((doc) => {
+                if (doc.exists) {
+                    var myTeamNum = doc.data().teamNumber;
+                    if (myTeamNum == 3) {
+                        var check = window.confirm("トンビに転生しますか？");
+                        if (check) {
+                            location.href = "./avatorTransfer/tr19.html";
+                        }
+                    }
+                }
+            })
+        }
+    })
 }
