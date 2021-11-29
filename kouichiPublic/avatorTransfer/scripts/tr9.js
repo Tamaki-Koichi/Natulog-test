040b55af8bd146ed48def03ca9a8db1005ddd4d6
ittiy <68366168+Tamaki-Koichi@users.noreply.github.com>
2021-11-29 16:30:07 +0900

    チーム・キャラクターの画面遷移、ユーザー情報更新用スクリプトの実装

---
+++ kouichiPublic/avatorTransfer/scripts/tr9.js
@@ -0,0 +1,63 @@
+'use strict';
+
+var db = firebase.firestore();
+
+function initApp() {
+    console.log('initApp()');
+    // Listening for auth state changes.
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in.
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var isAnonymous = user.isAnonymous;
+            var uid = user.uid;
+            var providerData = user.providerData;
+        } else {
+            // User is signed out.
+            location.href = './index.html';
+            toggleSignIn();
+        }
+    });
+}
+
+window.addEventListener = function() {
+    initApp();
+};
+
+function select() {
+    var avatorNum = 9;
+    var teamNum = 2;
+    var avatorName = "イルカ";
+    var teamName = "海の民";
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            var uid = user.uid;
+
+            var docRef = db.collection("Users");
+            docRef.doc(uid).update({
+                teamNumber: teamNum,
+                team: teamName,
+                avatorNumber: avatorNum,
+                avator: avatorName,
+            })
+        } else {
+            console.log('uid取得失敗');
+            //サインインできていないので登録画面に戻すようにしようかと
+        }
+    });
+    var teamPic = document.getElementById("teamPic");
+    teamPic.src = "../images/teampic/" + teamNum + ".jpg";
+    var avatorPic = document.getElementById("avatorPic");
+    avatorPic.src = "../images/avatorpic/" + avatorNum + ".png";
+    var transResult = document.getElementById("transResult");
+    transResult.innerHTML = `あなたは、${teamName}の${avatorName}に転生しました。`;
+
+}
+
+
+window.addEventListener = function() {
+    initApp();
+};
